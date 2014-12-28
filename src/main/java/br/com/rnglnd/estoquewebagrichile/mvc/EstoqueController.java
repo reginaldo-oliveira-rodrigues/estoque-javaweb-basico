f4882953be8a83761f00ecd38e345643818e8614
Reginaldo Rodrigues <regisolliver1@gmail.com>
2014-12-28 21:45:43 -0200

    Anotando o controlador web.

--- src/main/java/br/com/rnglnd/estoquewebagrichile/mvc/EstoqueController.java
+++ src/main/java/br/com/rnglnd/estoquewebagrichile/mvc/EstoqueController.java
@@ -1,9 +1,18 @@
 package br.com.rnglnd.estoquewebagrichile.mvc;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/estoque")
 public class EstoqueController {
     
-    public Object responderRequisicao(Object dados){
-        // TODO implementar
+    @RequestMapping(value = "/responder", method = RequestMethod.POST)
+    public @ResponseBody Object responderRequisicao(Object dados){
+        // TODO implementar com objetos de entrada e saída de dados 
+        // no formato JSON, que é DTO
         return new Object();
     }
 }
