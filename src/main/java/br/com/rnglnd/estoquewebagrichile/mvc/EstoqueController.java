4018e927a07d1d1d468d600ca6824b3ae579c11b
Reginaldo Rodrigues <regisolliver1@gmail.com>
2014-12-28 20:51:08 -0200

    Configurando Spring e criando o controlador web do Estoque.

---
+++ src/main/java/br/com/rnglnd/estoquewebagrichile/mvc/EstoqueController.java
@@ -0,0 +1,9 @@
+package br.com.rnglnd.estoquewebagrichile.mvc;
+
+public class EstoqueController {
+    
+    public Object responderRequisicao(Object dados){
+        // TODO implementar
+        return new Object();
+    }
+}
